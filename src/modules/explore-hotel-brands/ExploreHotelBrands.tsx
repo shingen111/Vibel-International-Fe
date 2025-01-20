f3885dc1ffb4ea1abd1808dbdb561a2d975cd137
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2025-01-20 11:16:09 +0700

    feat: update

--- src/modules/explore-hotel-brands/ExploreHotelBrands.tsx
+++ src/modules/explore-hotel-brands/ExploreHotelBrands.tsx
@@ -8,10 +8,9 @@ function ItemSection({ url }: { url: string }) {
   return (
     <div className="flex flex-col justify-center items-center w-[185px] h-[170px] border border-[#9B9B9B21] border-solid">
       <div
-        className="flex justify-center items-center bg-black"
+        className="flex justify-center items-center"
         style={{
-          height: 80,
-          width: 130,
+          width: 160,
         }}
       >
         <Image
@@ -27,29 +26,49 @@ function ItemSection({ url }: { url: string }) {
 }
 
 const DEFINE_LIST_BRAND = [
-  "/explore-hotel-brands/footer_alltra.svg",
-  "/explore-hotel-brands/footer_americ-inn-2.svg",
-  "/explore-hotel-brands/footer_baymont-2.svg",
-  "/explore-hotel-brands/footer_daysinn-1.svg",
-  "/explore-hotel-brands/footer_dazzler-2.svg",
-  "/explore-hotel-brands/footer_dolce-1.svg",
-  "/explore-hotel-brands/footer_echo-1.svg",
-  "/explore-hotel-brands/footer_hawthorn-2.svg",
-  "/explore-hotel-brands/footer_howard-johnson-1.svg",
-  "/explore-hotel-brands/footer_la-quinta.svg",
-  "/explore-hotel-brands/footer_microtel-1.svg",
-  "/explore-hotel-brands/footer_ramada-2.svg",
-  "/explore-hotel-brands/footer_registry.svg",
-  "/explore-hotel-brands/footer_super-8-1.svg",
-  "/explore-hotel-brands/footer_trademark-1.svg",
-  "/explore-hotel-brands/footer_travelodge-1.svg",
-  "/explore-hotel-brands/footer_tryp-1.svg",
-  "/explore-hotel-brands/footer_wyn-garden-1.svg",
-  "/explore-hotel-brands/footer_wyn-grand-1.svg",
-  "/explore-hotel-brands/footer_wyngate-1.svg",
-  "/explore-hotel-brands/footer-esplendor.svg",
-  "/explore-hotel-brands/Wyndham.svg",
-  
+  "/explore-hotel-brands/1.svg",
+  "/explore-hotel-brands/2.svg",
+  "/explore-hotel-brands/3.svg",
+  "/explore-hotel-brands/4.svg",
+  "/explore-hotel-brands/5.svg",
+  "/explore-hotel-brands/6.svg",
+  "/explore-hotel-brands/7.svg",
+  "/explore-hotel-brands/8.svg",
+  "/explore-hotel-brands/9.svg",
+  "/explore-hotel-brands/10.svg",
+  "/explore-hotel-brands/11.svg",
+  "/explore-hotel-brands/12.svg",
+  "/explore-hotel-brands/13.svg",
+  "/explore-hotel-brands/14.svg",
+  "/explore-hotel-brands/15.svg",
+  "/explore-hotel-brands/16.svg",
+  "/explore-hotel-brands/17.svg",
+  "/explore-hotel-brands/18.svg",
+  "/explore-hotel-brands/19.svg",
+  "/explore-hotel-brands/20.svg",
+  "/explore-hotel-brands/21.svg",
+  "/explore-hotel-brands/22.svg",
+  "/explore-hotel-brands/23.svg",
+  "/explore-hotel-brands/24.svg",
+  "/explore-hotel-brands/25.svg",
+  "/explore-hotel-brands/26.svg",
+  "/explore-hotel-brands/27.svg",
+  "/explore-hotel-brands/28.svg",
+  "/explore-hotel-brands/29.svg",
+  "/explore-hotel-brands/30.svg",
+  "/explore-hotel-brands/31.svg",
+  "/explore-hotel-brands/32.svg",
+  "/explore-hotel-brands/33.svg",
+  "/explore-hotel-brands/34.svg",
+  "/explore-hotel-brands/35.svg",
+  "/explore-hotel-brands/36.svg",
+  "/explore-hotel-brands/37.svg",
+  "/explore-hotel-brands/38.svg",
+  "/explore-hotel-brands/39.svg",
+  "/explore-hotel-brands/40.svg",
+  "/explore-hotel-brands/41.svg",
+  "/explore-hotel-brands/42.svg",
+  "/explore-hotel-brands/43.svg",
 ];
 
 export default function ExploreHotelBrands() {
